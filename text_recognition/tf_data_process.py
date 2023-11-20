56fabc67362f454b2381aa45c325fbf643e71502
dhaulagiri0 <yiqinw4@gmail.com>
2023-11-20 23:20:43 +0000

    images are now divided into patches instead of resized

--- text_recognition/tf_data_process.py
+++ text_recognition/tf_data_process.py
@@ -6,6 +6,10 @@ def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
+def _float_feature(value):
+    """Returns an float list from a bool / enum / int / uint."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
 def _bytes_feature(value):
     """Returns a bytes_list from a string / byte."""
     if isinstance(value, type(tf.constant(0))):
@@ -38,6 +42,37 @@ def getImage(img, height=224, width=224):
     img_tensor = tf.cast(tf.image.resize_with_pad(tensor, height, width), dtype=tf.uint8)
     return tf.io.encode_png(img_tensor)
 
+def getImageGrey(img, height=224, width=224):
+    tensor = tf.constant(tf.keras.utils.img_to_array(img))
+    tensor = tf.image.rgb_to_grayscale(tensor)
+    img_tensor = tf.image.resize_with_pad(tensor, target_height=height, target_width=width)
+    return img_tensor[tf.newaxis, :]
+
+# the average height and width of the images is around 4000x150
+def write_record_patches(entries, filename, patch_shape=(5, 5)):
+  
+    with tf.io.TFRecordWriter(filename) as writer:
+        for entry in entries:
+            img = Image.open(entry["img"])
+            input = entry["input"]
+            label = entry["label"]
+            try:
+                timg = getImageGrey(img, height=10, width=400)
+            except:
+                print(img.height, img.width)
+                continue
+            # extract 10x10 patches from the image as input sequence
+            patches = tf.image.extract_patches(timg, 
+                                               sizes=[1, patch_shape[0], patch_shape[1], 1], 
+                                               strides=[1, patch_shape[0], patch_shape[1], 1], 
+                                               rates=[1, 1, 1, 1], 
+                                               padding="VALID")
+            # squeeze to remove extra dim
+            values = [input, label, tf.io.serialize_tensor(tf.squeeze(patches))]
+            fs = [_int64_feature, _int64_feature, _bytes_feature]
+            keys = ["input", "label", "img"]
+            example = serialize_example(values, keys, fs)
+            writer.write(example)
 
 def write_record(entries, filename, limit=64):
   
