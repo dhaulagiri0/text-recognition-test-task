f7a2ddd6e6b891582de87bcb14015053a061c0b0
dhaulagiri0 <yiqinw4@gmail.com>
2023-11-26 09:21:01 +0000

    code clean up

--- text_recognition/data_creator.py
+++ text_recognition/data_creator.py
@@ -1,23 +1,311 @@
 
 """
 Current plan is to use some English dataset, maybe Maven, 
-and translate some of the texts to Chinese using python's translate library
+and translate some of the texts to Chinese using google/bing translate
 
 It is possible to translate each sentence fully or partially, 
 this would increase the amount of data available but perhaps at the 
 expense of the quality of the chinese dataset
 
-We can then generate images based on sentences using 
+I can then generate images based on sentences using 
 """
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-from bpemb import BPEmb
 import translators as ts
-import json, io, os
+import json, os
 import random
 from tqdm import tqdm
 import re
+
+"""
+A class to generate usable datasets from Maven
+The data generated (short_data) is of the following format:
+[
+ {
+    "img"      : path_to_image_data,
+    "tokens"   : tokenised_sequence,
+    "sequence" : raw_sequence (en, chi or mixed)
+ }
+]
+
+An intermediary list, base, is also generated of the
+following format:
+[
+ {
+    "sentence"   : sentence_in_English,
+    "translated" : sentence_in_Chinese,
+    "mixed"      : sentence_with_chi_and_en
+ }
+]
+The sequences in short_data are shorter pieces of texts 
+extracted from the sentences in base
+
+short_data needs to be further processed into tfrecords
+for model training (using tf_data_process.py)
+"""
+class JsonDataCreator():
+
+    def __init__(self,
+                 maven_path     = "dataset/",
+                 dataset_name   = "train",
+                 translate_rate = 0.3,
+                 vocab_path     = "dataset/vocab.json",
+                 img_path       = "dataset/test_imgs"):
+        
+        self.maven_path   = maven_path
+        self.dataset_name = dataset_name
+        self.img_path     = img_path
+
+        # this determine how much of an English sentence is translated
+        self.translate_rate = translate_rate
+
+        # get the base dataset
+        # which is a list of sentences
+        self.base = self.load_maven()
+
+        # check if vocabulary json is provided
+        # if not, make a new one
+        # the vocab file should be a dictionary of
+        # word to token
+        if vocab_path and os.path.isfile(vocab_path):
+            self.word_2_token = self._load_json_data(vocab_path)
+        else:
+            self.word_2_token = self.create_vocab()
+
+        self.vocab_size = len(self.word_2_token)
+
+    # simple method to laod a json file
+    def _load_json_data(self, path):
+        f = open(path)
+        l = json.load(f)
+        return l   
+
+    # dealing with the MAVEN dataset
+    def load_maven(self):
+
+        path = self.maven_path
+        dataset = self.dataset_name
+        sentences = []
+
+        with open(f"{path}/{dataset}.jsonl") as f:
+            total = 0
+            for i, line in enumerate(f):
+                total = max(total, i)
+                # I only need cur_dict["content"], these are the sentences in the wiki page
+                cur_dict = json.loads(line)
+                for sent_dict in cur_dict["content"]:
+                    sentence = sent_dict["sentence"]
+                    
+                    # translate to chinese
+                    translated = ts.translate_text(sentence, to_language='chi')
+                    # mix
+                    mixed      = self._mix_trans(sentence, self.translate_rate)
+
+                    sentences.append({"sentence"  : sentence,
+                                      "translated": translated,
+                                      "mixed"     : mixed})
+                if i > 1: break
+ 
+            print(f"loaded {total} articles for {dataset}. Totalling {len(sentences)} sentences")
+
+        return sentences
+    
+    """
+     randomly sample from two strings to create one string
+     rate determines the percentage of an English sentence being translated to Chinese
+     I chose to translate words individually since simply mixing a pair of 
+     English and Chinese sentence would destroy the meaning
+     This operation is quite resource intensive
+    """
+    def _mix_trans(self, en, rate):
+
+        def single_trans(word):
+            if random.uniform(0, 1) <= rate:
+                return ts.translate_text(word, to_language='chi')
+            else: 
+                return word
+
+        mixed = en.split(" ")
+        mixed = map(single_trans, mixed)
+        return " ".join(mixed)
+    
+    def create_vocab(self):
+        word_2_token = {}
+
+        unique_en = ""
+        unique_chi = ""
+
+        for entry in self.base:
+            en         = entry["sentence"]
+            unique_en  = ''.join(set(en + unique_en))
+
+            chi        = entry["translated"]
+            unique_chi = ''.join(set(chi + unique_chi))
+
+        token_id = 0
+        for en in sorted(unique_en):
+            word_2_token[en] = token_id
+            token_id += 1
+        
+        for chi in sorted(unique_chi):
+            word_2_token[chi] = token_id
+            token_id += 1
+        
+        # add the special tokens
+        word_2_token["<s>"]   = token_id
+        word_2_token["</s>"]  = token_id + 1
+        word_2_token["<unk>"] = token_id + 2
+        word_2_token[" "]     = token_id + 3
+
+        return word_2_token
+
+    def add_vocab(self, word):
+        if word not in self.word_2_token:
+            self.word_2_token[word] = len(self.word_2_token)
+    
+    def tokenise(self, sequence, add_to_dict=False):
+
+        word_2_token = self.word_2_token
+
+        tokens = []
+        for char in sequence:
+            if char in word_2_token:
+                token = word_2_token[char]
+            else:
+                print(f"{char} not found in dictionary")
+                if add_to_dict:
+                    word_2_token[char] = token = len(word_2_token)
+                else:
+                    token = word_2_token["<unk>"]
+            tokens.append(token)
+            
+        return tokens
+    
+    # exports an attribute as json
+    def export(self, path, attribute):
+
+        with open(path, "w") as f:
+            json.dump(attribute, f)
+    
+    # generate short sequences from base
+    """
+    using shorter sequences allows the model to generate
+    character sequences instead of words. This reduces
+    vocab size, increases flexibility and also allow the 
+    model to converge much faster
+    """
+    def gen_short(self, piece_length=2, create_imgs=True, fontsize=70):
+
+        base         = self.base
+        img_path     = self.img_path
+
+        short_data = []
+        cnt = 0
+        max_height = 0
+        max_width = 0
+        avg_height = 0
+        avg_wdith = 0
+
+        for entry in base:
+
+            en    = entry["sentence"]
+            en_tokens, en_pieces = self.split_sentence(en, piece_length)
+
+            chi_tokens   = []
+            chi_pieces   = []
+            mixed_tokens = []
+            mixed_pieces = []
+
+            if "translated" in entry:
+                chi   = entry["translated"]
+                mixed = entry["mixed"]
+
+                chi_tokens, chi_pieces     = self.split_sentence(chi, piece_length)
+                mixed_tokens, mixed_pieces = self.split_sentence(mixed, piece_length)
+
+            all_tokens = en_tokens + chi_tokens + mixed_tokens
+            all_pieces = en_pieces + chi_pieces + mixed_pieces
+            
+            # zip tokens and pieces and remove potential repeats
+            tokens_pieces = set([(" ".join(str(i) for i in k), v) for k ,v in zip(all_tokens, all_pieces)])
+
+            for pair in tokens_pieces:
+
+                # a dictionary to store data entries
+                d = {}
+
+                # generate random colours
+                c = tuple(random.sample(range(0, 255), 3))
+
+                # ensure there's white and dark background
+                if random.randint(0, 10) > 3:
+                    if random.randint(0, 10) > 3:
+                        c = (255, 255, 255)
+                    else:
+                        c = (30, 30, 30)
+                
+                if create_imgs:
+                    
+                    img = make_image(pair[1], 
+                                    fontsize, 
+                                    padding=10, 
+                                    image_background=c, 
+                                    text_color=tuple(255 - x for x in c))
+                    
+                    img.save(f"{img_path}/{self.dataset_name}/{cnt}.png")
+
+                    avg_height += img.height
+                    max_height = max(max_height, img.height)
+                    avg_wdith  += img.width
+                    max_width  = max(max_width, img.width)
+
+                # assume image already exists
+                d["img"]      = f"{img_path}/{self.dataset_name}/{cnt}.png"
+                d["tokens"]   = [int(x) for x in pair[0].split(" ")]
+                d["sequence"] = pair[1]
+
+                short_data.append(d)
+                cnt += 1
+
+        if create_imgs:
+            avg_wdith = avg_wdith // cnt
+            avg_height = avg_height // cnt
+
+            # some data about the images generated
+            print(f"Average image dimens: {avg_height}x{avg_wdith}")
+            print(f"Max image dimens:     {max_height}x{max_width}")
+        
+        self.short_data = short_data
+
+    # split a sentence into pieces in word tokens
+    # piece_length determines how long the pieces are
+    def split_sentence(self, sentence, piece_length, add_to_dict=False):
+        words = sentence.split(" ")
+        end = len(words)
+
+        tokens_list = []
+        pieces_list = []
+        # take generated_len number of words
+        for i in range(0, end, piece_length):
+            d = {}
+
+            if i + piece_length < end:
+                piece = words[i: i+piece_length]
+            else:
+                piece = words[i: end]
+
+            piece = " ".join(piece)
+            tokens_list.append(self.tokenise(piece, add_to_dict))
+            pieces_list.append(piece)
+        
+        return tokens_list, pieces_list
+
+
+
+### ------------- legacy functions ------------- ###
+
 
 def load_json_data(path="dataset/raw/test_slid.json"):
     f = open(path)
@@ -77,7 +365,6 @@ def create_dataset(json_path, set="train", img_path="dataset/images/", fontsize=
     return data
         
 
-
 #randomly sample from two strings to create one string
 # rate determines the percentage of an English sentence being translated to Chinese
 def mix_trans(en, rate):
@@ -101,7 +388,7 @@ def mix_translate(sentences, rate=0.3):
 # dealing with the MAVEN dataset
 def load_maven(path="dataset", dataset="train"):
     sentences = []
-    # multibpemb = BPEmb(lang="multi", vs=1000000, dim=300)
+    
     with open(os.path.join(path, f"{dataset}.jsonl")) as train_file:
         total = 0
         for i, line in enumerate(train_file):
@@ -144,7 +431,7 @@ def get_words_dict(entries, d, return_unknown=False):
     d["<UNK>"] = id + 3
     return unknowns
 
-# the dataset obtained does not contain curly braces or new lines, we I am manually inserting some of these characters
+# the dataset obtained does not contain curly braces or new lines, I am manually inserting some of these characters
 def insert_char(sentences, characters="{}\n"):
     rate = 0.05
     for sentence in sentences:
@@ -173,10 +460,6 @@ def make_vocab(entries, path="dataset/vocab.json"):
     with open(path, 'w') as j:
         json.dump(d, j)
 
-# def load_json_entries(path="dataset/raw/", dataset="train"):
-#     f = open(path + dataset)
-#     l = json.load(f)
-#     return l     
 
 def check_piece(pieces, d):
     ps = []
@@ -210,7 +493,6 @@ def slide_sentence(pieces, tokens, limit=64, window = 5):
         new_t.append(tokens[(len(pieces) - limit):])
 
     return new_p, new_t
-
 
 
 # process all sentences, add BOS and EOS, split sequences longer than 512
